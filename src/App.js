1048fb4e92bd6f166dc2391e3aa85dc9b05b65a1
Tiana Husted <tiana.husted@gmail.com>
2020-10-01 16:01:14 -0700

    Creating route for Alertify page

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Home from './pages/Home';
 import Contact from './pages/Contact';
 import WebDev from './pages/WebDev';
 import InteractiveArt from './pages/InteractiveArt';
+import Alertify from "./pages/projects/Alertify";
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
           <Route exact path="/contact" component={Contact} />
           <Route exact path="/webdevelopment" component={WebDev} />
           <Route exact path="/interactiveart" component={InteractiveArt} />
+          <Route exact path="/alertify" component={Alertify} />
         </Wrapper>
       </div>
     </Router>
