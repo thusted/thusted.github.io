8f04a8002b7232f89db5d849388a1b863c5aea13
Tiana Husted <tiana.husted@gmail.com>
2020-10-01 16:36:52 -0700

    Creating page for employeeSummary project

---
+++ src/pages/projects/EmployeeSummary.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class EmployeeSummary extends Component {
+  render() {
+    return (
+      <div>
+        EmployeeSummary
+      </div>
+    );
+  }
+}
+
+export default EmployeeSummary;
