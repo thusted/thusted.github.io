c638b52b7ded08748394f9ca1c3ac20fc2346a05
Tiana Husted <tiana.husted@gmail.com>
2020-10-01 16:41:17 -0700

    Adding comments to file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
-
 import { BrowserRouter as Router, Route } from "react-router-dom";
+
+//Importing css
 import "./App.css";
 import "materialize-css/dist/css/materialize.min.css";
+
+//Importing components
 import Navbar from "./components/Navbar";
 import Wrapper from "./components/Wrapper";
+
+// Importing pages
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import WebDev from "./pages/WebDev";
