96816d975f3c94c95c9db1730c583a5ae9032672
Tiana Husted <tiana.husted@gmail.com>
2020-10-01 15:42:43 -0700

    Importing and adding card images

--- src/pages/WebDev.js
+++ src/pages/WebDev.js
@@ -1,4 +1,10 @@
 import React, { Component } from "react";
+import Alertify from "../images/alertify.png";
+import Burger from "../images/burgerApp.png";
+import JobSearch from "../images/jobSearch.png";
+import EmployeeSummary from "../images/employeeSummary.png";
+import WeatherDashboard from "../images/weatherDashboard.png";
+import CodeQuiz from "../images/codeQuiz.png";
 
 class WebDev extends Component {
   render() {
@@ -16,7 +22,7 @@ class WebDev extends Component {
               <div class="col s12">
                 <div class="card">
                   <div class="card-image">
-                    <img src="assets/images/alertify.png" alt="alertify"/>
+                    <img src={Alertify} alt="alertify"/>
                   </div>
                   <div class="card-content">
                     <p><b>Alertify</b> is an application that can provide users with additional security in case of an emergency. If something happened and in need of an immediate rescue, send an alert with Alertify mobile tracker.</p>
@@ -33,7 +39,7 @@ class WebDev extends Component {
               <div class="col s12">
                 <div class="card">
                   <div class="card-image">
-                    <img src="assets/images/burgerApp.png" alt="burger"/>
+                    <img src={Burger} alt="burger"/>
                   </div>
                   <div class="card-content">
                     <p><b>Burgers!</b> is an application allows users to log burgers with MySQL, Node, Express, Handlebars, and a homemade ORM.</p>
@@ -50,7 +56,7 @@ class WebDev extends Component {
               <div class="col s12">
                 <div class="card">
                   <div class="card-image">
-                    <img src="assets/images/jobSearch.png" alt="jobSearch"/>
+                    <img src={JobSearch} alt="jobSearch"/>
                   </div>
                   <div class="card-content">
                     <p><b>Software Developers Job Search</b> is an application designed for developers out of school who are looking for jobs.</p>
@@ -67,7 +73,7 @@ class WebDev extends Component {
               <div class="col s12">
                 <div class="card">
                   <div class="card-image">
-                    <img src="assets/images/employeeSummary.png" alt="employeeSummary"/>
+                    <img src={EmployeeSummary} alt="employeeSummary"/>
                   </div>
                   <div class="card-content">
                     <p><b>Employee Summary</b> is a command-line application that generates an HTML file that displays information on an engineeering team based on user input.</p>
@@ -79,29 +85,12 @@ class WebDev extends Component {
               </div>
             </div>
             <br/>
-          
+
             <div class="row">
               <div class="col s12">
                 <div class="card">
                   <div class="card-image">
-                    <img src="assets/images/readmeGen.png" alt="readmeGen"/>
-                  </div>
-                  <div class="card-content">
-                    <p><b>README Generator</b> is a command-line application that dynamically generates a professional README.md from a user's input.</p>
-                  </div>
-                  <div class="card-action">
-                    <a href="assets/projects/readmeGen.html">VIEW PROJECT</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <br/>
-          
-            <div class="row">
-              <div class="col s12">
-                <div class="card">
-                  <div class="card-image">
-                    <img src="assets/images/weatherDashboard.png" alt="weatherDashboard"/>
+                    <img src={WeatherDashboard} alt="weatherDashboard"/>
                   </div>
                   <div class="card-content">
                     <p><b>Weather Dashboard</b> is an application designed to show the weather outlook for any given city. When a user searches for a city, they are presented with current and future conditions for that city and that city is added to the search history.</p>
@@ -118,7 +107,7 @@ class WebDev extends Component {
               <div class="col s12">
                 <div class="card">
                   <div class="card-image">
-                    <img src="assets/images/codeQuiz.png" alt="codeQuiz"/>
+                    <img src={CodeQuiz} alt="codeQuiz"/>
                   </div>
                   <div class="card-content">
                     <p><b>Code Quiz</b> is an application created to run an interactive timed quiz with Javascript fundamental questions.</p>
