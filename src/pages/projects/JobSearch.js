b2a02e14e857947aa9a2ff7422564a301f8f5196
Tiana Husted <tiana.husted@gmail.com>
2020-10-01 16:22:21 -0700

    Creating page for jobsearch project

---
+++ src/pages/projects/JobSearch.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class JobSearch extends Component {
+  render() {
+    return (
+      <div>
+        JobSearch
+      </div>
+    );
+  }
+}
+
+export default JobSearch;
