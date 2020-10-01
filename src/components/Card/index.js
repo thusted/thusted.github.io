8121517908e2ccd0cdabc35dc104e05277fc2d46
Tiana Husted <tiana.husted@gmail.com>
2020-10-01 16:09:45 -0700

    Converting card component from class to function

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,31 +1,29 @@
-import React, { Component } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 
-class Card extends Component {
-  render({imgSrc, title, description, link}) {
-    return (
-      <div class="row">
-        <div class="col s12">
-          <div class="card">
-            <div class="card-image">
-              <img src={imgSrc} alt="cardImg"/>
-            </div>
-            <div class="card-content">
-              <p><b>{title}</b> {description}</p>
-            </div>
-            <div class="card-action">
-              <Link
-                to={link}
-                className={window.location.pathname === {link} ? "nav-link active" : "nav-link"}
-              >
-                VIEW PROJECT
-              </Link>
-            </div>
+function Card ({imgSrc, title, description, link}) {
+  return (
+    <div class="row">
+      <div class="col s12">
+        <div class="card">
+          <div class="card-image">
+            <img src={imgSrc} alt="cardImg"/>
+          </div>
+          <div class="card-content">
+            <p><b>{title}</b> {description}</p>
+          </div>
+          <div class="card-action">
+            <Link
+              to={link}
+              className={window.location.pathname === {link} ? "nav-link active" : "nav-link"}
+            >
+              VIEW PROJECT
+            </Link>
           </div>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default Card;
