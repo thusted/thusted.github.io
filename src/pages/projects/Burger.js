89b1191dd918b67f0a135e07662d7f716df72808
Tiana Husted <tiana.husted@gmail.com>
2020-10-01 16:14:20 -0700

    Creating page for burger app

---
+++ src/pages/projects/Burger.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Burger extends Component {
+  render() {
+    return (
+      <div>
+        Burger
+      </div>
+    );
+  }
+}
+
+export default Burger;
