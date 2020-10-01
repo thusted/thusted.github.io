42ffa137d558702ce8e13df78069326c2f856db8
Tiana Husted <tiana.husted@gmail.com>
2020-10-01 15:59:57 -0700

    Creating projects folder with Alertify.js

---
+++ src/pages/projects/Alertify.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Alertify extends Component {
+  render() {
+    return (
+      <div>
+        alertify
+      </div>
+    );
+  }
+}
+
+export default Alertify;
