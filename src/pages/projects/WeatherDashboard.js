b6e508d4fca3f5219278e2756083d9d68f226b6c
Tiana Husted <tiana.husted@gmail.com>
2020-10-01 16:45:18 -0700

    Creating page for WeatherDashboard project

---
+++ src/pages/projects/WeatherDashboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class WeatherDashboard extends Component {
+  render() {
+    return (
+      <div>
+        WeatherDashboard
+      </div>
+    );
+  }
+}
+
+export default WeatherDashboard;
