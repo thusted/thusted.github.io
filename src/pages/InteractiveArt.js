a8071d01525546cbc0e1d5ab46fa2251bc87f453
Tiana Husted <tiana.husted@gmail.com>
2020-10-03 00:11:15 -0700

    Importing all necessary images and components

--- src/pages/InteractiveArt.js
+++ src/pages/InteractiveArt.js
@@ -1,5 +1,16 @@
 import React, { Component } from "react";
 
+// Importing images
+import MovingDolls from "../images/movingDolls.jpg";
+import Dollhouse from "../images/dollhouse.jpg";
+import SonicSteps from "../images/sonicSteps.JPG";
+import Particles from "../images/particlesOfTheVoice.png";
+import Binaries from "../images/binaries.png";
+
+// Importing components
+import Project from "../components/Project";
+import Header from "../components/Header";
+
 class InteractiveArt extends Component {
   render() {
     return (
