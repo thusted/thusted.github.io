b8e43c3bbb173a1b1419d2ae2ce9fe7975c8e38a
Tiana Husted <tiana.husted@gmail.com>
2020-10-03 03:00:40 -0700

    Removing view project button and making each project a hoverable link

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,25 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+//Import css
+import "./style.css";
+
 function Project ({className, imgSrc, title, description, link}) {
   return (
     <div className={className}>
-      <div className="card">
-        <div className="card-image">
-          <img src={imgSrc} alt="cardImg"/>
+      <Link
+        to={link}
+        className={window.location.pathname === {link} ? "nav-link active" : "nav-link"}
+      >
+        <div className="card hoverable">
+          <div className="card-image">
+            <img src={imgSrc} alt="cardImg"/>
+          </div>
+          <div className="card-content">
+            <p><b>{title}</b> {description}</p>
+          </div>
         </div>
-        <div className="card-content">
-          <p><b>{title}</b> {description}</p>
-        </div>
-        <div className="card-action">
-          <Link
-            to={link}
-            className={window.location.pathname === {link} ? "nav-link active" : "nav-link"}
-          >
-            VIEW PROJECT
-          </Link>
-        </div>
-      </div>
+      </Link>
     </div>
   )
 }
