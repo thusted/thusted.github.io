d2c62378036f43980c6feee0fe3f8d972e99ad89
Tiana Husted <tiana.husted@gmail.com>
2020-10-02 22:59:00 -0700

    Removing div with class row

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -3,23 +3,21 @@ import { Link } from "react-router-dom";
 
 function Project ({imgSrc, title, description, link}) {
   return (
-    <div class="row">
-      <div class="col s12">
-        <div class="card">
-          <div class="card-image">
-            <img src={imgSrc} alt="cardImg"/>
-          </div>
-          <div class="card-content">
-            <p><b>{title}</b> {description}</p>
-          </div>
-          <div class="card-action">
-            <Link
-              to={link}
-              className={window.location.pathname === {link} ? "nav-link active" : "nav-link"}
-            >
-              VIEW PROJECT
-            </Link>
-          </div>
+    <div class="col s12">
+      <div class="card">
+        <div class="card-image">
+          <img src={imgSrc} alt="cardImg"/>
+        </div>
+        <div class="card-content">
+          <p><b>{title}</b> {description}</p>
+        </div>
+        <div class="card-action">
+          <Link
+            to={link}
+            className={window.location.pathname === {link} ? "nav-link active" : "nav-link"}
+          >
+            VIEW PROJECT
+          </Link>
         </div>
       </div>
     </div>
