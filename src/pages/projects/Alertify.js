9ec90e1fa9a9d64807196d7eb100358b88b45cd3
Tiana Husted <tiana.husted@gmail.com>
2020-10-03 16:20:25 -0700

    Adding header to project

--- src/pages/projects/Alertify.js
+++ src/pages/projects/Alertify.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
 
+//Importing components
+import Header from "../../components/Header";
+
 class Alertify extends Component {
   render() {
     return (
-      <div>
-        alertify
+      <div className="container">
+        <div className="section">
+          <Header title="ALERTIFY" subtitle="AN APPLICATION THAT CAN PROVIDE USERS WITH ADDITIONALL SECURITY IN CASE OF AN EMERGENCY"/>
+        </div>
       </div>
+      
     );
   }
 }
