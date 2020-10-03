48897632f58a182650b74c3d181cd815b4753c84
Tiana Husted <tiana.husted@gmail.com>
2020-10-03 01:09:14 -0700

    Creating page for particles project

---
+++ src/pages/projects/Particles.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Particles extends Component {
+  render() {
+    return (
+      <div>
+        Particles
+      </div>
+    );
+  }
+}
+
+export default Particles;
