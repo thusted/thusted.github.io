c809bc3f614f2951c09e2e1096833a5f26c98c71
Tiana Husted <tiana.husted@gmail.com>
2020-10-02 23:50:05 -0700

    Creating project page for dollhouse

---
+++ src/pages/projects/Dollhouse.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Dollhouse extends Component {
+  render() {
+    return (
+      <div>
+        Dollhouse
+      </div>
+    );
+  }
+}
+
+export default Dollhouse;
