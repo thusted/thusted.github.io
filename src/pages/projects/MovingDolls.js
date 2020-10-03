309a052df25fb387640df2a679c65440d9cf4943
Tiana Husted <tiana.husted@gmail.com>
2020-10-03 00:38:44 -0700

    Creating project page for The Moving Dolls

---
+++ src/pages/projects/MovingDolls.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class MovingDolls extends Component {
+  render() {
+    return (
+      <div>
+        MovingDolls
+      </div>
+    );
+  }
+}
+
+export default MovingDolls;
