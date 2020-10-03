b21bc84bf9188becdf774246998d972fe41a9195
Tiana Husted <tiana.husted@gmail.com>
2020-10-03 00:58:27 -0700

    Creating project page for sonic steps

---
+++ src/pages/projects/SonicSteps.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class SonicSteps extends Component {
+  render() {
+    return (
+      <div>
+        SonicSteps
+      </div>
+    );
+  }
+}
+
+export default SonicSteps;
