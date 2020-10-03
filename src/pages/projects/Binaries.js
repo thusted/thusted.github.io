b21f3eab48ef457b18b3fa92a887b30a4dabbb0d
Tiana Husted <tiana.husted@gmail.com>
2020-10-03 01:16:57 -0700

    Creating page for binaries project

---
+++ src/pages/projects/Binaries.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Binaries extends Component {
+  render() {
+    return (
+      <div>
+        Binaries
+      </div>
+    );
+  }
+}
+
+export default Binaries;
