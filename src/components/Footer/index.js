77a2160588d18c21d3d898daad1280b55919537f
Tiana Husted <tiana.husted@gmail.com>
2021-02-09 14:58:06 -0800

    Changing all instances of 'class' to 'className' for consistency

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -28,36 +28,36 @@ function Footer() {
   return(
     <footer className="page-footer">
       <div id="footerContainer" className="row">
-        <div class="col s12 m6 l6">
-          <div class="card body" id="contactCard">
+        <div className="col s12 m6 l6">
+          <div className="card body" id="contactCard">
             <form id="mail">
-              <div class="form-group">
+              <div className="form-group">
                 <label for="msgInput">Don't Be Shy, Say Hi! Fill out the text box to send me an email.</label>
                 <textarea type="text" class="form-control" id="msgInput" rows="5"></textarea>
               </div>
               <br/>
-              <button class="btn waves-effect" type="submit" name="action" onClick={composeEmail}>
-                <i class="material-icons">send</i>
+              <button className="btn waves-effect" type="submit" name="action" onClick={composeEmail}>
+                <i className="material-icons">send</i>
               </button>
             </form>
           </div>
         </div>
 
-        <div class="col s12 m6 l6">
-          <div class="pagesContainer">
-            <div class="col s4 m4 l4">
+        <div className="col s12 m6 l6">
+          <div className="pagesContainer">
+            <div className="col s4 m4 l4">
               <a href="https://github.com/thusted">
                 <img className="hoverable" id="github" src={Github} alt="githubImg"/>
               </a>
             </div>
 
-            <div class="col s4 m4 l4">
+            <div className="col s4 m4 l4">
               <a href="https://www.linkedin.com/in/tiana-husted-395057163/">
                 <img className="hoverable" id="linkedin" src={Linkedin} alt="linkedImg"/>
               </a>
             </div>
 
-            <div class="col s4 m4 l4">
+            <div className="col s4 m4 l4">
               <a href={Resume}>
                 <img className="hoverable" id="resume" src={ResumeIcon} alt="resumeImg"/>
               </a>
