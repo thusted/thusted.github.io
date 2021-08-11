38657e06f9d0938bd484057ec1b8fb3d3b6fd367
Tiana Husted <tiana.husted@gmail.com>
2021-08-10 17:26:46 -0700

    Changing image for second parallax on homepage, taking content from past projects section and integrating it into second parallax, removing third parallax from homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -124,63 +124,58 @@ function Home() {
       </div>
 
       <div className="parallax-container valign-wrapper">
-        <div className="parallax"><img src={Dollhouse2} alt="Unsplashed background img 2"/></div>
-      </div>
+        <div className="parallax"><img src={BlinkBackground2} alt="Unsplashed background img 2"/></div>
+        <div className="container">
+          <div className="section">
+            <Header title="FEATURED PROJECTS" subtitle="PAST WORK"/>
+            <br/>
+            
+            <div className="row">
+              <div id="alertifyProject" className="col s12 m4 l4">
+                <Project href="#modal1" id="modal1" imgSrc={Alertify}title="Alertify" modalDescription="Alertify can provide users with additional security in case of an emergency. If something happened and in need of an immediate rescue, send an alert with Alertify mobile tracker. User will sign up to create an account. Once created, they can login to provide an emergency contact to be alerted if an emergency situation. Within their login, they have a red button to press (Alertify) that sends out a text to their contact with current location to be found with." imageArray={alertifyImages} tech={["HTML", "CSS","JavaScript", "JQuery", "NodeJS", "Sequelize","Passport","BCrypt", "Quick Easy SMS API"]} links={["https://alertifynow.herokuapp.com/", "https://github.com/thusted/Alertify"]}/>
 
-      <div className="container">
-        <div className="section">
-          <Header title="FEATURED PROJECTS" subtitle="PAST WORK"/>
-          <br/>
-          
-          <div className="row">
-            <div id="alertifyProject" className="col s12 m4 l4">
-              <Project href="#modal1" id="modal1" imgSrc={Alertify}title="Alertify" modalDescription="Alertify can provide users with additional security in case of an emergency. If something happened and in need of an immediate rescue, send an alert with Alertify mobile tracker. User will sign up to create an account. Once created, they can login to provide an emergency contact to be alerted if an emergency situation. Within their login, they have a red button to press (Alertify) that sends out a text to their contact with current location to be found with." imageArray={alertifyImages} tech={["HTML", "CSS","JavaScript", "JQuery", "NodeJS", "Sequelize","Passport","BCrypt", "Quick Easy SMS API"]} links={["https://alertifynow.herokuapp.com/", "https://github.com/thusted/Alertify"]}/>
+                <button className="btn-large hoverable">
+                  <Link
+                    id="alertifyProject"
+                    style={{color:"white"}} 
+                    to="/webdevelopment"
+                    className={window.location.pathname === "/webdevelopment" ? "nav-link active" : "nav-link"}
+                  >
+                    ALL WEB DEVELOPEMENT
+                  </Link>
+                </button>
+              </div>
 
-              <button className="btn-large hoverable">
+              <div id="dollhouseProject" className="col s12 m4 l4">
+                <Project href="#modal2" id="modal2" imgSrc={DollhouseBack2} title="The Dollhouse" modalDescription="Modeled after a turn of the century Victorian home, The Dollhouse was a piece comissioned for and featured at Burning Man 2019. The back side of the home had the illusion of being completely exposed to open playa, but was enclosed with clear acrylic paneling. This allowed citizens of Black Rock City to see the illuminated structure, and the participants within, from afar. There are two stories, each with two rooms, conventional to the Victorian era: ballroom, kitchen, bathroom, and bedroom. Art pieces in the home include photographs and paintings of a family long past, ghostly porcelain dolls, oddities, remnants of a seance gone wrong, an infinity mirror, and window etchings, all of which react to human touch and/or presence. We hoped to create an ethereal world on playa, captivating and enticing people to play and discover more secrets within." imageArray={dollhouseImages} tech={["Arduino Uno", "Arduino IDE", "MaxMSP", "Kyma", "MadMapper", "Capacitive Touch Sensors","Servo Motors", "GameTrak", "PPG Heart Sensor", "Relay Modules"]} links={["https://www.youtube.com/watch?v=W9TY_xQfr4Y&t=23s", "https://www.youtube.com/watch?v=FfcdDc0tSdU"]}/>
+
+                
                 <Link
-                  id="alertifyProject"
+                  id="dollhouseProject"
                   style={{color:"white"}} 
-                  to="/webdevelopment"
-                  className={window.location.pathname === "/webdevelopment" ? "nav-link active" : "nav-link"}
+                  to="/interactiveart"
+                  className={window.location.pathname === "/interactiveart" ? "nav-link active" : "nav-link"}
                 >
-                  ALL WEB DEVELOPEMENT
+                  <button className="btn-large hoverable">ALL INTERACTIVE ART</button>
                 </Link>
-              </button>
-            </div>
+              </div>
 
-            <div id="dollhouseProject" className="col s12 m4 l4">
-              <Project href="#modal2" id="modal2" imgSrc={DollhouseBack2} title="The Dollhouse" modalDescription="Modeled after a turn of the century Victorian home, The Dollhouse was a piece comissioned for and featured at Burning Man 2019. The back side of the home had the illusion of being completely exposed to open playa, but was enclosed with clear acrylic paneling. This allowed citizens of Black Rock City to see the illuminated structure, and the participants within, from afar. There are two stories, each with two rooms, conventional to the Victorian era: ballroom, kitchen, bathroom, and bedroom. Art pieces in the home include photographs and paintings of a family long past, ghostly porcelain dolls, oddities, remnants of a seance gone wrong, an infinity mirror, and window etchings, all of which react to human touch and/or presence. We hoped to create an ethereal world on playa, captivating and enticing people to play and discover more secrets within." imageArray={dollhouseImages} tech={["Arduino Uno", "Arduino IDE", "MaxMSP", "Kyma", "MadMapper", "Capacitive Touch Sensors","Servo Motors", "GameTrak", "PPG Heart Sensor", "Relay Modules"]} links={["https://www.youtube.com/watch?v=W9TY_xQfr4Y&t=23s", "https://www.youtube.com/watch?v=FfcdDc0tSdU"]}/>
+              <div id="bezierProject" className="col s12 m4 l4">
+                <Project href="#modal3" id="modal3" imgSrc={House} title="Bezier Data Visualization" modalDescription="Bezier Data Visualization uses bezier curves to plot data, using government CSV files from 2017. This sketch allows users to see a visual representation of how often (on a scale of percentage) the members of the Senate and House vote in line with Trump’s position."  imageArray={bezierImages} tech={["Processing"]} links={["https://github.com/thusted/Bezier-Data-Visualization", "https://youtu.be/Pd81FM694fs"]}/>
 
-              
-              <Link
-                id="dollhouseProject"
-                style={{color:"white"}} 
-                to="/interactiveart"
-                className={window.location.pathname === "/interactiveart" ? "nav-link active" : "nav-link"}
-              >
-                <button className="btn-large hoverable">ALL INTERACTIVE ART</button>
-              </Link>
-            </div>
-
-            <div id="bezierProject" className="col s12 m4 l4">
-              <Project href="#modal3" id="modal3" imgSrc={House} title="Bezier Data Visualization" modalDescription="Bezier Data Visualization uses bezier curves to plot data, using government CSV files from 2017. This sketch allows users to see a visual representation of how often (on a scale of percentage) the members of the Senate and House vote in line with Trump’s position."  imageArray={bezierImages} tech={["Processing"]} links={["https://github.com/thusted/Bezier-Data-Visualization", "https://youtu.be/Pd81FM694fs"]}/>
-
-              
-              <Link
-                id="bezierProject"
-                style={{color:"white"}} 
-                to="/processing"
-                className={window.location.pathname === "/processing" ? "nav-link active" : "nav-link"}
-              >
-                <button className="btn-large hoverable">ALL PROCESSING(JAVA)</button>
-              </Link>
+                
+                <Link
+                  id="bezierProject"
+                  style={{color:"white"}} 
+                  to="/processing"
+                  className={window.location.pathname === "/processing" ? "nav-link active" : "nav-link"}
+                >
+                  <button className="btn-large hoverable">ALL PROCESSING(JAVA)</button>
+                </Link>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-
-      <div className="parallax-container valign-wrapper">
-        <div className="parallax"><img src={Dollhouse3} alt="Unsplashed background img 3"/></div>
       </div>
     </div>
   );
