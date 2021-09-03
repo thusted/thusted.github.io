d0d5fa4212a14f6ba0384a0fbfec45b2f9c47888
Tiana Husted <tiana.husted@gmail.com>
2021-09-02 19:41:03 -0700

    Adding map method to Material Box component so the component can accept an image array of any size and create a materialbox image for each item

--- src/components/MaterialBox/index.js
+++ src/components/MaterialBox/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 
+// Importing style
+import "./style.css";
+
 function MaterialBox ({imageArray}) {
   if(imageArray) {
     return (
       <div className="hide-on-med-and-up">
-        <div>Hi</div>
+        {imageArray.map((image) => (
+          <img className="materialboxed responsive-img" alt="projectImg" src={image}></img>
+        ))}
       </div>
     ); 
   } else {
