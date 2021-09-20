a10ffe7a07a198e81c0065bb43977e703f50a4bb
Tiana Husted <tiana.husted@gmail.com>
2021-09-19 17:26:12 -0700

    Creating project for David Hanig website

--- src/pages/WebDesign.js
+++ src/pages/WebDesign.js
@@ -16,6 +16,13 @@ import Ahfemme10 from "../images/ahfemme10.jpeg";
 import Ahfemme11 from "../images/ahfemme11.jpeg";
 import Ahfemme12 from "../images/ahfemme12.jpeg";
 import Ahfemme13 from "../images/ahfemme13.jpeg";
+import DavidCover from "../images/davidCover.jpeg";
+import David1 from "../images/david1.jpeg";
+import David2 from "../images/david2.jpeg";
+import David3 from "../images/david3.jpeg";
+import David4 from "../images/david4.jpeg";
+import David5 from "../images/david5.jpeg";
+import David6 from "../images/david6.jpeg";
 
 // Importing components
 import Project from "../components/Project";
@@ -23,6 +30,7 @@ import Header from "../components/Header";
 
 //Image arrays
 const ahfemmeImages = [Ahfemme1, Ahfemme2, Ahfemme3, Ahfemme4, Ahfemme5, Ahfemme6, Ahfemme7, Ahfemme8, Ahfemme9, Ahfemme10, Ahfemme11, Ahfemme12, Ahfemme13];
+const davidImages = [David1, David2, David3, David4, David5, David6];
 
 function WebDesign() {
   useEffect(() => {
@@ -40,6 +48,7 @@ function WebDesign() {
         <div className="section">
           <div className="row">
             <Project href="#modal17" id="modal17" className="col s12 m4 l4" imgSrc={AhfemmeCover} title="AHFEMME" modalDescription="AHFEMME is a queer art collective, birthed from the cosmos. They use a variety of mediums to produce multi-sensory interactive performances and events. This website was created for the artists to let the people know who they are and what they're doing, as well as donate to current and future projects."  imageArray={ahfemmeImages} tech={["Designed and created with Squarespace"]} links={["https://www.ahfemme.com"]}/>
+            <Project href="#modal18" id="modal18" className="col s12 m4 l4" imgSrc={DavidCover} title="DAVID HANIG" modalDescription="David Hanig is an author and poet, based out of Washington state. David wanted a simple design to primarily help promote the launch of his new book. Additionally, he wanted sections to showcase his poetic works, in both written blog and video form."  imageArray={davidImages} tech={["Designed and created with WordPress"]} links={["https://www.davidhanig.com"]}/>
           </div>
         </div>
       </div>
